3880e94412cb99988d906c52759af301b46eeb40
Podoprosvetov Dmitry <ditansu@gmail.com>
2017-11-09 01:40:42 +0300

    test 31

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         
-        let testVersion = 30
+        let testVersion = 31
         
         get("hello") { req in
             var json = JSON()
