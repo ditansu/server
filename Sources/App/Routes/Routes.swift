ff77ffa9c66d473bb564d361c7f856e57cc7dfa5
Podoprosvetov Dmitry <ditansu@gmail.com>
2017-11-13 00:14:06 +0300

    check hook 01

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         
-        let testVersion = 51
+        let testVersion = 52
       
         get("hello") { req in
             var json = JSON()
