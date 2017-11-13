d4293eff2fb852d7735844fc52e58f58321655ca
Podoprosvetov Dmitry <ditansu@gmail.com>
2017-11-13 17:26:21 +0300

    refactoring #1

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         
-        let testVersion = 56
+        let testVersion = 57
       
         get("hello") { req in
             var json = JSON()
